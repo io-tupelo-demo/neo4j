11c5a6eff99495492cae9fd4f3dce59167e41186
Alan Thompson <cloojure@gmail.com>
2021-08-26 18:03:55 -0700

    misc

--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -8,8 +8,8 @@
     [java.net URI]))
 
 (def driver
-  (db/connect (URI. "bolt://localhost:7687")  ; uri
-              "neo4j" "secret")); user/pass
+  (db/connect (URI. "bolt://localhost:7687") ; uri
+    "neo4j" "secret")) ; user/pass
 
 (dotest   ; -focus
   (util/with-connection
@@ -17,46 +17,38 @@
     ; "neo4j+s://4ca9bb9b.databases.neo4j.io" "neo4j" "g6o2KIftFE6EIYMUCIY9a6DW0oVcwihh7m0Z5DP-jcY"
 
     (is= (util/neo4j-info) {:name "Neo4j Kernel" :version "4.3.3" :edition "enterprise"})
-    (is= (util/neo4j-version)  "4.3.3")
+    (is= (util/neo4j-version) "4.3.3")
     (is= (util/apoc-version) "4.3.0.0")
 
     ; deleted users in DB from previous run
-      (util/delete-all-nodes! )
+    (util/delete-all-nodes!)
 
-      (is (util/apoc-installed?))
+    (is (util/apoc-installed?))
 
-      ; Using a session
+    ; Using a session
     (util/with-session
       ; tests consume all output within the session lifetime, so don't need `doall`, `vec`, or `unlazy`
-      (let [create-user-cypher "CREATE (u:User $User)
-                                return u as newb"]
-        (is= (util/session-run create-user-cypher {:User {:first-name "Luke" :last-name "Skywalker"}})
+      (let [create-user-cmd "CREATE (u:User $User)  return u as newb"]
+        (is= (util/session-run create-user-cmd {:User {:first-name "Luke" :last-name "Skywalker"}})
           [{:newb {:first-name "Luke" :last-name "Skywalker"}}])
-        (is= (util/session-run create-user-cypher {:User {:first-name "Leia" :last-name "Organa"}})
+        (is= (util/session-run create-user-cmd {:User {:first-name "Leia" :last-name "Organa"}})
           [{:newb {:first-name "Leia" :last-name "Organa"}}])
-        (is= (util/session-run create-user-cypher {:User {:first-name "Anakin" :last-name "Skywalker"}})
+        (is= (util/session-run create-user-cmd {:User {:first-name "Anakin" :last-name "Skywalker"}})
           [{:newb {:first-name "Anakin" :last-name "Skywalker"}}])
-
         (is= 3 (count (util/all-nodes)))))
 
     (comment
       ; Using a transaction
-      (let [get-all-users-cmd
-                   "MATCH (u:User)
-                    RETURN u as UZZER"
-            result (db/with-transaction driver
-                     tx
-                     ; or vec/doall to realize output within tx life
-                     (unlazy
-                       (util/session-run  get-all-users-cmd)
-                       ))]
+      (let [get-all-users-cmd "MATCH (u:User)  RETURN u as UZZER"
+            result            (util/with-session
+                                (unlazy ; or vec/doall to realize output within session life
+                                  (util/session-run get-all-users-cmd)))]
         (is= result
           [{:UZZER {:first-name "Luke" :last-name "Skywalker"}}
            {:UZZER {:first-name "Leia" :last-name "Organa"}}
            {:UZZER {:first-name "Anakin" :last-name "Skywalker"}}]))
 
-
-      (is= [{:batches 1 :total 3}] ; delete all users from DB
+      (is= [{:batches 1 :total 3}] ; delete all users from DB via APOC
         (util/delete-all-nodes! driver)))
 
     ))
